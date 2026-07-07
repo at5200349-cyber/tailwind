d291f57efb87f50fc709360807df59b243d170fc
at5200349-cyber <at5200349@gmail.com>
2026-07-07 22:04:55 +0530

    company grid added in fotter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Campanis from "./components/Campanis";
 function App() {
   return (
  <>
 <Navbar/>
 <Hero/>
+<Campanis/>
 </>
   );
 }
---
+++ src/components/Campanis.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Campanis() {
+    const companies=[
+        "Google","Microsoft","Amazon","Netflix","Meta","OpenAI"
+    ];
+
+  return (
+    <>
+  
+     <section className="py-20 bg-gray-800">
+      <div className="max-w-7xl mx-auto px-6">
+
+        
+        <h2 className="text-center text-purple-500 uppercase tracking-widest font-semibold mb-12">
+          Trusted by 10,000+ Developers Worldwide
+        </h2>
+
+      
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-8 place-items-center">
+
+          {companies.map((company, index) => (
+            <div
+              key={index}
+              className="text-3xl font-bold text-blue-500 hover:text-purple-600 hover:scale-110 transition-transform duration-300 cursor-pointer"
+            >
+              {company}
+            </div>
+          ))}
+
+        </div>
+      </div>
+    </section>
+    
+    </>
+  )
+}
